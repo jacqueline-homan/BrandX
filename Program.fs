2bca0851500a7d62d5b13ace7a62dde85f785e78
Evan Cofsky <evan@theunixman.com>
2016-12-20 00:26:21 -0600

    Fix redoubling from shithub. Add gitignore.

--- Program.fs
+++ Program.fs
@@ -10,8 +10,7 @@ let path = @"204-MGCTLYST-BLNJ-16542577549-2.txt"
 let file = File.ReadAllText(path)
 *)
 [<EntryPoint>]
-let main argv = 
+let main argv =
     test pISARec "ISA*00*          *00*          *ZZ*MGCTLYST       *02*BLNJ           *160930*1453*U*00401*000000001*0*P*:~" |> printfn "%A"
 
     0 // return an integer exit code
-
