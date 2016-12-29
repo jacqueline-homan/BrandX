572cbd37bae716e31308fe24cf032d363daa75d3
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-12-29 14:34:03 -0500

    Added ST header

---
+++ ST.fs
@@ -0,0 +1,11 @@
+﻿module BrandX.ST
+
+open FParsec
+open System.Collections.Generic
+open System.IO
+open BrandX.Structures
+
+type TrnsSetIdCode = 
+    | MotorCarrierLoadTender
+
+let pTrnsSetIdCode : Parser<TrnsSetIdCode> = skipString "M" >>. preturn MotorCarrierLoadTender .>> pFSep
