ec1f06d41f9e11fda648613543fe64af61721c85
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-12-29 21:58:13 -0500

    started on ST portion

--- EDI.fs
+++ EDI.fs
@@ -2,6 +2,7 @@
 
 open BrandX.GS
 open BrandX.ISA
+open BrandX.ST
 open FParsec
 
 let pEDI = pipe2 pISARec pGS (fun i g -> (i, g))
--- ST.fs
+++ ST.fs
@@ -5,7 +5,21 @@ open System.Collections.Generic
 open System.IO
 open BrandX.Structures
 
+//ST-01
 type TrnsSetIdCode = 
     | MotorCarrierLoadTender
 
 let pTrnsSetIdCode : Parser<TrnsSetIdCode> = skipString "M" >>. preturn MotorCarrierLoadTender .>> pFSep
+
+//ST-02
+type TrnSetCtrlNo = 
+    | TrnSetCtrlNo of string 
+
+let pCode : Parser<TrnSetCtrlNo> = anyString 3 |>> TrnSetCtrlNo .>> pFSep
+
+type TransCode =
+    { idCode : TrnsSetIdCode
+      ctrlNo : TrnSetCtrlNo}
+
+let pctrl : Parser<TrnSetCtrlNo> = manyMinMaxSatisfy 4 9 Char.IsDigit .>> pFSep |>> TrnSetCtrlNo 
+    
