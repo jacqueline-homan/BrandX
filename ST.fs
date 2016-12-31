32bbcc1d50c891301c3d9f63df8d465397b9a8c6
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-12-31 18:56:19 -0500

    added data structure for ST portion

--- ST.fs
+++ ST.fs
@@ -6,39 +6,24 @@ open System.Collections.Generic
 open System.IO
 open BrandX.Structures
 
-//ST-01
-type TrnsSetIdCode = 
+type TsID = 
     | MotorCarrierLoadTender
 
-let pTrnsSetIdCode : Parser<TrnsSetIdCode> = skipString "204" >>. preturn MotorCarrierLoadTender //.>> pFSep
+let pTsId : Parser<TsID> = skipString "204" >>. preturn MotorCarrierLoadTender .>> pFSep
 
-//ST-02
-type TrnSetCtrlNo = 
-    | TrnSetCtrlNo of string 
+type TctrlNo =
+    | TctrlNo of string 
 
-let pCode : Parser<TrnSetCtrlNo> = anyString 3 |>> TrnSetCtrlNo //.>> pRSep
+let pTctrlNo : Parser<TctrlNo> = manyMinMaxSatisfy 4 9 (fun c -> isDigit c ) |>> TctrlNo .>> pRSep  
 
-type TransCode =
-    { idCode : TrnsSetIdCode
-      ctrlNo : TrnSetCtrlNo}
-
-let pctrl : Parser<TrnSetCtrlNo> = manyMinMaxSatisfy 4 9 Char.IsDigit .>> pFSep |>> TrnSetCtrlNo 
-
-let pTransCode = 
-    pipe2 pTrnsSetIdCode pCode (fun t c ->
-        {idCode = t
-         ctrlNo = c}) 
-        
-type ST = ST of TrnsSetIdCode * TrnSetCtrlNo * TransCode
-
+type ST = ST of TsID * TctrlNo 
 let pST = 
-    skipString "ST" >>. pFSep >>. pTrnsSetIdCode
-    >>= fun p -> 
-        pCode
-        >>= fun q ->
-            pctrl
-            >>= fun r ->
-                pTransCode
-                >>= fun s ->
-                preturn (ST(p, q, s))
-    
+    skipString "ST" >>. pFSep
+    >>= fun _ -> 
+        pTsId
+        >>= fun id ->
+            pTctrlNo 
+            >>= fun ctrl ->
+                preturn (ST(id, ctrl))
+
+ 
---
+++ ScatchWork.fsx
@@ -0,0 +1,37 @@
+﻿
+//ST-01
+type TrnsSetIdCode = 
+    | MotorCarrierLoadTender
+
+let pTrnsSetIdCode : Parser<TrnsSetIdCode> = skipString "204" >>. preturn MotorCarrierLoadTender .>> pFSep
+
+//ST-02
+type TrnSetCtrlNo = 
+    | TrnSetCtrlNo of string 
+
+let pCode : Parser<TrnSetCtrlNo> = anyString 3 |>> TrnSetCtrlNo .>> pRSep
+
+type TransCode =
+    { idCode : TrnsSetIdCode
+      ctrlNo : TrnSetCtrlNo}
+
+let pctrl : Parser<TrnSetCtrlNo> = manyMinMaxSatisfy 4 9 (fun c -> isDigit c || isAsciiLetter c || isAnyOf "~" c) .>> pFSep |>> TrnSetCtrlNo 
+
+let pTransCode = 
+    pipe2 pTrnsSetIdCode pCode (fun t c ->
+        {idCode = t
+         ctrlNo = c}) 
+        
+type ST = ST of TrnsSetIdCode * TrnSetCtrlNo * TransCode
+
+let pST = 
+    skipString "ST" >>. pFSep >>. pTrnsSetIdCode
+    >>= fun p -> 
+        pCode
+        >>= fun q ->
+            pctrl
+            >>= fun r ->
+                pTransCode
+                >>= fun s ->
+                preturn (ST(p, q, s))
+    
