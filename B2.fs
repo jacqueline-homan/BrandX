6c3fb905c646f6b1ea69f0b41b6164f18ce4d338
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-01-01 01:46:23 -0500

    started on B2 fields

--- B2.fs
+++ B2.fs
@@ -6,3 +6,20 @@ open System.Collections.Generic
 open System.IO
 open BrandX.Structures
 
+//Beginning Segment for Shipment Information Transaction
+
+type StdCarAlphaCode = 
+    | StdCarAlphaCode of string 
+
+let pAlpha : Parser<StdCarAlphaCode> = manyMinMaxSatisfy 2 4 (fun c -> isDigit c) |>> StdCarAlphaCode .>> pFSep
+
+type ShipIdNo = 
+    | ShipIdNo of string 
+
+let pShip : Parser<ShipIdNo> = manyMinMaxSatisfy 1 30 (fun c -> isDigit c || isAsciiLetter c) |>> ShipIdNo .>> pFSep
+
+type ShipPmt = 
+    | Collect of ShipPmt
+    | Prepaid of ShipPmt
+    | ThirdPartyPay of ShipPmt
+
