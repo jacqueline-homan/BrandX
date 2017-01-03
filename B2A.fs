45f48b647673699d0fad3edee46e2a80ff29249f
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-01-03 15:55:42 -0500

    first attempt on B2A EDI record

---
+++ B2A.fs
@@ -0,0 +1,31 @@
+﻿module BrandX.B2A
+
+open FParsec
+open BrandX.Structures
+open System
+open System.Collections.Generic
+open System.IO
+
+//B2A: Transaction Set Purpose Code
+type SetPurpCode =
+    | Original
+    | Cancellation
+    | Change 
+
+let pSetPurpCode : Parser<SetPurpCode> =
+    (skipString "00" >>? preturn Original)
+    <|> (skipString "01" >>? preturn Cancellation)
+    <|> (skipString "04" >>? preturn Change)
+
+type AppType = 
+    | AppType of string
+
+let pAppType : Parser<Option<AppType>> = 
+    opt 
+        (anyString 2 |>> AppType) .>> pFSep
+
+type B2A = SetPurpCode * Option<AppType> 
+
+let pB2A : Parser<B2A> = 
+    skipString "B2A" >>. pFSep
+    >>. tuple2 pSetPurpCode pAppType .>> pRSep
--- EDI.fs
+++ EDI.fs
@@ -1,9 +1,10 @@
 ﻿module BrandX.EDI
 
 open BrandX.B2
+open BrandX.B2A
 open BrandX.GS
 open BrandX.ISA
 open BrandX.ST
 open FParsec
 
-let pEDI = pISARec .>>. pGS .>>. pST .>>. pB2
+let pEDI = pISARec .>>. pGS .>>. pST .>>. pB2 .>>. pB2A
