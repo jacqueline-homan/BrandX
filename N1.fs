6f8bdb37befcc4dc5b831dcb903aaea107fd8898
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-01-05 15:24:43 -0500

    building skeleton for the N1 parser group

--- N1.fs
+++ N1.fs
@@ -7,7 +7,7 @@ open BrandX.Structures
 
 let pEntity : Parser<_> = manyMinMaxSatisfy 2 3 Char.IsLetterOrDigit
 
-let pName : Parser<_> = manyMinMaxSatisfy 1 60 
+let pName : Parser<_> = manyMinMaxSatisfy 1 60 (isNoneOf "*~") 
 
 let pN1 : Parser<_> = 
     skipString "N1" >>. pFSep 
