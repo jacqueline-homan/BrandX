6a0c3d3c342b086705adfc8a9bf53aa38b920a8c
Evan Cofsky <evan@theunixman.com>
2017-01-05 14:43:34 -0600

    * EDI.fs: 	  Add strawman N1 type to parse.
    	* N1.fs:
    	  Create strawman N1 type and parser.
    
    	This will parse the "N1" record tag, then the field separator,
    	  and then return an N1.

--- N1.fs
+++ N1.fs
@@ -1,11 +1,11 @@
 ﻿module BrandX.N1
 
+open BrandX.Structures
+open FParsec
 open System
 open System.IO
-open FParsec
-open BrandX.Structures
 
-type Entity = 
+type Entity =
     | Entity of string
 let pEntity : Parser<_> = manyMinMaxSatisfy 2 3 isAsciiLetter |>> Entity
 
@@ -16,10 +16,10 @@ let pName : Parser<_> = manyMinMaxSatisfy 1 60 (isNoneOf "*~") |>> Name
 type IdQual =
     | IdQual of string
 
-let pIdQual : Parser<_> = manyMinMaxSatisfy 1 2 isDigit |>> IdQual 
+let pIdQual : Parser<_> = manyMinMaxSatisfy 1 2 isDigit |>> IdQual
 
 type IdCode =
-    | IdCode of string 
+    | IdCode of string
 
 let pIdCode : Parser<_> = manyMinMaxSatisfy 2 80 (isNoneOf "*~") |>> IdCode
 
@@ -27,11 +27,11 @@ let pIdCode : Parser<_> = manyMinMaxSatisfy 2 80 (isNoneOf "*~") |>> IdCode
 type N1 =
     | N1 of Entity * Name //* IdQual * IdCode
 
-let pN1 : Parser<_> = 
+let pN1 : Parser<_> =
     skipString "N1" >>. pFSep >>. pEntity .>>. pName |>> N1
     (*
         >>= fun n ->
-            pName 
+            pName
             >>= fun q ->
                 pIdQual
                 >>= fun c ->
@@ -39,5 +39,3 @@ let pN1 : Parser<_> =
                     >>= fun d ->
                         preturn (N1(n, q, c, d))
     *)
-
-
