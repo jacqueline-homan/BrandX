39ae0e4c229d3a26e31a4008fb4f2926cee6b5bd
Evan Cofsky <evan@theunixman.com>
2017-01-05 18:13:12 -0600

    Add field separators, use tuple2.
    - We need field separators for sure, at least between fields.
    - `tuple2` will pass the results from each parser as a tuple, and then
      we hand it off to N1 with |>> to call N1 in the parser and return
      that value.

--- N1.fs
+++ N1.fs
@@ -5,31 +5,32 @@ open FParsec
 open System
 open System.IO
 
-type Entity =
+type Entity = 
     | Entity of string
+
 let pEntity : Parser<_> = manyMinMaxSatisfy 2 3 isAsciiLetter |>> Entity
 
-type Name =
+type Name = 
     | Name of string
+
 let pName : Parser<_> = manyMinMaxSatisfy 1 60 (isNoneOf "*~") |>> Name
 
-type IdQual =
+type IdQual = 
     | IdQual of string
 
 let pIdQual : Parser<_> = manyMinMaxSatisfy 1 2 isDigit |>> IdQual
 
-type IdCode =
+type IdCode = 
     | IdCode of string
 
 let pIdCode : Parser<_> = manyMinMaxSatisfy 2 80 (isNoneOf "*~") |>> IdCode
 
-
-type N1 =
+type N1 = 
     | N1 of Entity * Name //* IdQual * IdCode
 
-let pN1 : Parser<_> =
-    skipString "N1" >>. pFSep >>. pEntity .>>. pName |>> N1
-    (*
+let pN1 : Parser<N1> = 
+    skipString "N1" >>. pFSep >>. tuple2 (pEntity .>> pFSep) pName |>> N1
+(*
         >>= fun n ->
             pName
             >>= fun q ->
