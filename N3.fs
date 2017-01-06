f3d4ceb713f9ef0037cc658146ea35a62d9d6c1c
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-01-06 14:30:04 -0500

    added N3 parser group

--- N3.fs
+++ N3.fs
@@ -8,18 +8,22 @@ open BrandX.Structures
 type AddressInfo =
     | AddressInfo of string
 
-let pAddy : Parser<AddressInfo> = manyMinMaxSatisfy 1 55 (isNoneOf "*~") |>> AddressInfo //.>> pFSep
+let pAddy : Parser<AddressInfo> = manyMinMaxSatisfy 1 55 (isNoneOf "*~") |>> AddressInfo .>> pFSep
 
 type Details = 
     | Details of string 
 
 let pDet : Parser<Details option> = 
     (opt 
-        (manyMinMaxSatisfy 1 55 (isNoneOf "*~") 
-         |>> (fun x -> (* WTF do I put here to get this working for a string type? *)  |> Details))) //.>> pFSep
+        (manyMinMaxSatisfy 1 55 (isNoneOf "*~") |>> Details)) .>> pRSep
 
 type N3 = 
     | N3 of AddressInfo * Details option
 
 let pN3 : Parser<N3> = 
-    skipString "N3" .>> pFSep .>> tuple2 (pAddy .>> pFSep) (pDet .>> pRSep) |>> N3 
+    //skipString "N3" >>. pFSep >>. tuple2 (pAddy .>> pFSep) (pDet .>> pFSep) |>> N3 .>> pRSep
+    skipString "N3" >>. pFSep >>. pAddy 
+    >>= fun a ->
+        pDet 
+        >>= fun b ->
+            preturn (N3(a, b)) //.>> pRSep
