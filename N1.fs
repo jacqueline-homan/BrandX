b872429dd4e722d1ca5a234637040603fcebd4e2
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-01-05 21:09:31 -0500

    still trying here...

--- N1.fs
+++ N1.fs
@@ -7,28 +7,28 @@ open BrandX.Structures
 
 type Entity = 
     | Entity of string
-let pEntity : Parser<_> = manyMinMaxSatisfy 2 3 isAsciiLetter |>> Entity
+let pEntity : Parser<_> = manyMinMaxSatisfy 2 3 isAsciiLetter |>> Entity .>> pFSep
 
 type Name =
     | Name of string
-let pName : Parser<_> = manyMinMaxSatisfy 1 60 (isNoneOf "*~") |>> Name
+let pName : Parser<_> = manyMinMaxSatisfy 1 60 (isNoneOf "*~") |>> Name .>> pFSep
 
 type IdQual =
     | IdQual of string
 
-let pIdQual : Parser<_> = manyMinMaxSatisfy 1 2 isDigit |>> IdQual 
+let pIdQual : Parser<_> = manyMinMaxSatisfy 1 2 isDigit |>> IdQual .>> pFSep
 
 type IdCode =
     | IdCode of string 
 
-let pIdCode : Parser<_> = manyMinMaxSatisfy 2 80 (isNoneOf "*~") |>> IdCode
+let pIdCode : Parser<_> = manyMinMaxSatisfy 2 80 (isNoneOf "*~") |>> IdCode .>> pFSep
 
 
 type N1 =
-    | N1 of Entity * Name //* IdQual * IdCode
+    | N1 of Entity * Name //* IdQual //* IdCode
 
 let pN1 : Parser<_> = 
-    skipString "N1" >>. pFSep >>. pEntity .>>. pName |>> N1
+    skipString "N1" >>. pFSep >>. tuple2 (pEntity >>. pFSep) pName |>> N1
     (*
         >>= fun n ->
             pName 
