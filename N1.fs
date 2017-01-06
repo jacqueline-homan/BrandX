03248491e4785465a48c48cca1e9855fab31c969
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-01-05 23:09:39 -0500

    N1 parser group completely finished

--- N1.fs
+++ N1.fs
@@ -7,37 +7,38 @@ open BrandX.Structures
 
 type Entity = 
     | Entity of string
-let pEntity : Parser<Entity> = manyMinMaxSatisfy 2 3 isAsciiLetter |>> Entity //.>> pFSep
+let pEntity : Parser<Entity> = manyMinMaxSatisfy 2 3 isAsciiLetter |>> Entity .>> pFSep
 
 type Name =
     | Name of string
-let pName : Parser<Name> = manyMinMaxSatisfy 1 60 (isNoneOf "*~") |>> Name //.>> pFSep
+let pName : Parser<Name> = manyMinMaxSatisfy 1 60 (isNoneOf "*~") |>> Name .>> pFSep
 
 type IdQual =
     | IdQual of string
 
-let pIdQual : Parser<IdQual> = manyMinMaxSatisfy 1 2 isDigit |>> IdQual //.>> pFSep
+let pIdQual : Parser<IdQual> = manyMinMaxSatisfy 1 2 isDigit |>> IdQual .>> pFSep
 
 type IdCode =
     | IdCode of string 
 
-let pIdCode : Parser<IdCode> = manyMinMaxSatisfy 2 80 (isNoneOf "*~") |>> IdCode //.>> pFSep
+let pIdCode : Parser<IdCode> = manyMinMaxSatisfy 2 80 (isNoneOf "*~") |>> IdCode .>> pRSep
 
 
 type N1 =
     | N1 of Entity * Name * IdQual * IdCode
 
+(*
 let pN1 : Parser<N1> = 
     skipString "N1" >>. pFSep >>. tuple4 (pEntity .>> pFSep) ( pName .>> pFSep) (pIdQual .>> pFSep) (pIdCode .>> pRSep)|>> N1
-    (*
+*)
+//because I did not want a nasty tuple4 monster staring me down, I refactored
+let pN1 : Parser<N1> =
+    skipString "N1" >>. pFSep >>. pEntity   
         >>= fun n ->
             pName 
             >>= fun q ->
-                pIdQual
-                >>= fun c ->
+                pIdQual 
+                >>= fun c -> 
                     pIdCode
-                    >>= fun d ->
-                        preturn (N1(n, q, c, d))
-    *)
-
-
+                    >>= fun x -> 
+                        preturn (N1(n, q, c, x))
