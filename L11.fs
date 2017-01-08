258dad2e3f13f4d5df9c19c14f0edf030e0a5cf5
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-01-08 09:25:42 -0500

    second retry on L11 segment

--- L11.fs
+++ L11.fs
@@ -8,10 +8,38 @@ open FParsec
 
 type RefId = 
     | RefId of string 
-let pRefId : Parser<RefId> = manyMinMaxSatisfy 1 30 (isNoneOf "*~**:***.") |>> RefId .>> pFSep
 
+let pRefId : Parser<RefId> = manyMinMaxSatisfy 1 30 (isNoneOf "*~**:' '***'.") |>> RefId .>> pFSep
+
+
+type RefQual = 
+    | RefQual of string 
+
+let pRefQual : Parser<RefQual> = manyMinMaxSatisfy 2 3 (isNoneOf "*~**:' '***'.") |>> RefQual .>> pFSep
+
+
+type BusInstruct = 
+    { referenceId : RefId
+      referenceQual : RefQual}
+
+let pBus = 
+    pipe2 pRefId pRefQual (fun i q -> {referenceId = i; referenceQual = q}) 
+
+
+type Description =
+    | Description of string 
+
+let pDesc : Parser<Description> = manyMinMaxSatisfy 1 80 (isNoneOf "*~**:' '") |>> Description .>> pFSep
+       
 type L11 =
-    | L11 of RefId
+    | L11 of BusInstruct * Description
 
-let pL11 : Parser<L11> =
-    skipString "L11" .>> pFSep .>> pRefId 
+let pL11Record = 
+    pBus 
+    >>= fun x ->
+        pDesc 
+        >>= fun y -> 
+        preturn (L11(x, y))
+
+let pL11 = 
+    skipString "L11" >>. pFSep >>. pL11Record .>> pRSep 
